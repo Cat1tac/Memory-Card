a2a86d36e34632df6739a5b93abb10cb859e5c57
Cat1tac <waynepowell235@gmail.com>
2025-05-20 03:48:27 -0400

    No longer loads twice in strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import Randomize from './components/randomize'
 import './App.css'
 
+
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [saved, setSaved] = useState(new Set());
 
   return (
     <>
-      <h1>Pokemon Memory Game</h1>
-      <Randomize />
+      <div>
+          <p>Score: {score}</p>
+          <p>Best Score: {bestScore}</p>
+      </div>
+      <h1>Poké Memory</h1>
+      <Randomize 
+        score={score}
+        setScore={setScore}
+        bestScore={bestScore}
+        setBestScore={setBestScore}
+        saved={saved}
+        setSaved={setSaved}
+        />
     </>
   )
 }
--- src/components/randomize.jsx
+++ src/components/randomize.jsx
@@ -1,49 +1,69 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, cache } from "react";
 
-export default function Randomize(){
+export default function Randomize({score, setScore, bestScore, setBestScore, saved, setSaved}){
     const [pokemon, setPokemon] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     
-
+    //Make into function so can call it in the next function
     useEffect(() => {
-        const dexNumSet = new Set([]);
+        let ignore = false;
+        const dexNumSet = [];
 
         //Populate Set With unique dex numbers
         for(let i = 0; i < 10; i++){
             let dexNum = Math.floor(Math.random() * (386 - 252) + 252);
-            while (dexNumSet.has(dexNum)){
+            while (dexNumSet.includes(dexNum)){
                 dexNum = Math.floor(Math.random() * (386 - 252) + 252);
             }
-            dexNumSet.add(dexNum);
+            dexNumSet.push(dexNum);
         }
-
-        for(const dexNum of dexNumSet){
-            fetch(`https://pokeapi.co/api/v2/pokemon/${dexNum}`, {mode: "cors"})
+        
+        for(let i = 0; i < dexNumSet.length; i++){
+            cache(fetch(`https://pokeapi.co/api/v2/pokemon/${dexNumSet[i]}`, {mode: "cors"})
             .then((response) => {
                 if (response.status >= 400) {
-                    throw new Error("server error");
+                   throw new Error
                 }
                 return response.json();
-            }) //add conditional that changes pokemon if the link does not work
-            .then((response) => setPokemon(pokemon => [...pokemon, {dex: dexNum, link:response.sprites.versions["generation-v"]["black-white"].animated.front_default}]))
+            }) 
+            .then((response) => {
+               if(!ignore){
+                    setPokemon(pokemon => [...pokemon, {dex: dexNumSet[i], link:response.sprites.versions["generation-v"]["black-white"].animated.front_default}]) 
+                }
+            })
             .catch((error) => setError(error))
-            .finally(() => setLoading(false))
+            .finally(() => setLoading(false)))
+        }
+
+        return () => {
+            ignore = true;
         }
     }, []);
         
+    
     if (loading) return <p>Loading Images</p>
     if (error) return <p>{error}</p>
+
+    function handleReset(e) {
+        if(!saved.has(e.target.id)){
+            setSaved(saved.add(e.target.id));
+            setScore(score + 1);
+        } else {
+            setBestScore(bestScore >= score ? bestScore : score);
+            setScore(score = 0);
+            saved.clear();
+            setSaved(saved);
+            Randomize({score, setScore, bestScore, setBestScore, saved, setSaved})
+        }
+    }
 
     return (
         <div>
             {pokemon.map((pocketmon) => {
-                return <img key={pocketmon.dex} src={pocketmon.link} onClick={Reset}/>
+                return <img key={pocketmon.dex} src={pocketmon.link} id={pocketmon.dex} onClick={handleReset}/>
             })}
         </div>
     )
 }
 
-function Reset() {
-    
-}
